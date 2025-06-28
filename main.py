b148c1ee486b8ee8295dd45b73857d8499c18dc8
Tyler Pearson <tyler.a.pear@gmail.com>
2025-06-27 23:42:55 -0500

    reads book path as cli arg 2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stats import get_alpha_character_counts_sorted, get_word_count
+import sys
+
+args = sys.argv
 
 def get_book_text(book):
   with open(book) as f:
@@ -9,18 +12,22 @@ def get_word_count(text):
   return len(text_array)
 
 def main():
-  book = get_book_text('./books/frankenstein.txt')
-  data_list = get_alpha_character_counts_sorted(book)
+  if len(args) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  else:
+    book = get_book_text(args[1])
+    data_list = get_alpha_character_counts_sorted(book)
 
-  print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
-  print("----------- Word Count ----------")
-  print(f"Found {get_word_count(book)} total words")
-  print("--------- Character Count -------")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {args[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_word_count(book)} total words")
+    print("--------- Character Count -------")
 
-  for item in data_list:
-    print(f"{item["name"]}: {item["num"]}")
+    for item in data_list:
+      print(f"{item["name"]}: {item["num"]}")
 
-  print("============= END ===============")
+    print("============= END ===============")
 
 main()
